5f6c7e90dee600a08d60061428b2a7c445e03868
Irfan <irfanmehmud140@gmail.com>
2026-03-15 19:30:49 +0530

    feat: implement streaming feature

--- frontend.py
+++ frontend.py
@@ -22,17 +22,22 @@ user_input = st.chat_input('Type here')
 
 if user_input:
 
-    # first add the message to message_history
+    #  add the  user message to message_history
     st.session_state['message_history'].append({'role': 'user', 'content': user_input})
     with st.chat_message('user'):
         st.text(user_input)
 
-    # Invoking the workflow
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
-    
-    ai_message = response['messages'][-1].content
-    
-    # first add the message to message_history
-    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+
+    #  add assistant message to  message_history
     with st.chat_message('assistant'):
-        st.text(ai_message)
+       ai_message = st.write_stream(
+            message_chunk.content for  message_chunk, metadata in chatbot.stream(
+                {
+                    "messages": [HumanMessage(content=user_input)]
+                },
+                config=CONFIG,
+                stream_mode="messages"
+            )
+        )
+       
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
